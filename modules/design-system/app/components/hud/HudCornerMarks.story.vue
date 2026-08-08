b80695161dbc9d6ea13f6092d8389b7900b9f58a
chatondearu <823314+chatondearu@users.noreply.github.com>
2026-08-08 18:32:42 +0200

    feat(design-system): add HudCornerMarks primitive
    Move priority-zone corner brackets into the HUD component family.

---
+++ modules/design-system/app/components/hud/HudCornerMarks.story.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HudCornerMarks from './HudCornerMarks.vue'
+</script>
+
+<template>
+  <Story title="HUD / HudCornerMarks">
+    <Variant title="Default">
+      <div class="bg-background p-8">
+        <div class="relative h-32 w-64 border border-outline_variant/20">
+          <HudCornerMarks />
+        </div>
+      </div>
+    </Variant>
+  </Story>
+</template>
